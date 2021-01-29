cea71936b65c1c45aad5e1f0d29a7adaa6183032
pradip pack <pradip.pack@gmail.com>
2021-01-29 11:09:32 +0000

    intial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+JavaScript history/releases
+How to  run javascript using browser/nodejs
+Data types
+String
+Conditons stament
+Loops
+Functions
+Arrays
+Objects
+Classes
+Export/Import
+
+2009 - ES5 ECMASCRIPT
+2015 - ES6 / ECMA SCRIPT 2015
+ES6+ ES NEXT
+
+BABEL INTERPRETER/ COMPILE ES6 ES
+
---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+console.log('hello world');
+alert('hi')
+
+var x = 5;
+var y = "hi";
+var z = true;
+
+console.log(x, y, z)
+
+
+const x = 10;
+
+{
+    x = 5
+    console.log(x + x)
+}
+
+console.log(x)
+
+*/
+
+let fName = "John Smith";
+
+console.log(fName.length)
+console.log(fName.charAt(3))
+console.log(fName.slice(0,3))
+console.log(fName.substring(0,2))
+
+
